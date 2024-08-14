d15becaabbd19506a75e73c5e8e1664a61cd2356
caspa <example@example.com>
2024-08-14 22:59:29 +0300

    simplified perspective camera implementation

--- src/engine/core/cameras/PerspectiveCamera/PerspectiveCamera.ts
+++ src/engine/core/cameras/PerspectiveCamera/PerspectiveCamera.ts
@@ -5,8 +5,8 @@ import { Camera, ICameraProps } from '../Camera/Camera'
 export interface PerspectiveCameraProps extends ICameraProps {}
 
 export class PerspectiveCamera extends Camera {
-    private _pitch = 0
-    private _yaw = 0
+    public pitch = 0
+    public yaw = 0
 
     constructor(props: PerspectiveCameraProps) {
         super(props)
@@ -15,7 +15,7 @@ export class PerspectiveCamera extends Camera {
         const targetVec = props?.target ?? vec3.copy(positionVec)
         const back = vec3.normalize(vec3.sub(positionVec, targetVec))
         this._recalculateAngles(back)
-        this.position = positionVec
+        super.position = positionVec
     }
 
     private _recalculateAngles(direction: Vec3) {
@@ -23,37 +23,16 @@ export class PerspectiveCamera extends Camera {
         this.pitch = -Math.asin(direction[1])
     }
 
-    get pitch() {
-        return this._pitch
-    }
-
-    get yaw() {
-        return this._yaw
-    }
-
-    set pitch(value) {
-        this._pitch = value
-    }
-
-    set yaw(value) {
-        this._yaw = value
-    }
-
-    // set matrix(value: Mat4) {
-    //     super.matrix = value
-    //     this._recalculateAngles(this.back)
-    // }
-
     update(): Mat4 {
         this.yaw = Stuff.mod(this.yaw, Math.PI * 2)
         this.pitch = Stuff.clamp(this.pitch, -Math.PI / 2, Math.PI / 2)
 
-        const position = vec3.copy(this.position)
+        const position = vec3.copy(super.position)
         super.matrix = mat4.rotateX(mat4.rotationY(this.yaw), this.pitch)
         super.position = position
 
         super.view = mat4.invert(super.matrix)
 
-        return this.view
+        return super.view
     }
 }
