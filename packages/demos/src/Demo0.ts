4a235351060ab580085e37fa5acc0a2182b9e849
caspa2289 <65313374+caspa2289@users.noreply.github.com>
2024-12-25 15:00:57 +0300

    models reworked && wip

--- packages/demos/src/Demo0.ts
+++ packages/demos/src/Demo0.ts
@@ -1,67 +1,83 @@
 import { vec3 } from 'wgpu-matrix'
 import shaderCode from '../shaders/testShader.wgsl'
 import { WheezyGLBLoader, Scene, IGameObject } from '@wheezy/engine'
-
-const stuffNames = [
-    'sun',
-    'earth',
-    'uranus',
-    'jupiter',
-    'mercury',
-    'venus',
-    'mars',
-    'saturn',
-    'neptune',
-]
+import { ArcBallCamera } from '@wheezy/engine/src/engine/core/cameras/ArcBallCamera'
 
 export class Demo0 extends Scene {
     private _shaderModule: GPUShaderModule
 
-    //FIXME: The vertex count on these fuckers is insane
+    //FYI: Always remember to reset orientation in blender :/
     private sun!: IGameObject //==> OK, needs optimization
 
-    private earth!: IGameObject //==> OK, but unnecessary transforms present
-    private uranus!: IGameObject //==> OK, but unnecessary transforms present
-    private jupiter!: IGameObject //==> OK, needs optimization
-    private mercury!: IGameObject //==> OK, but missing slobs of stuff, MODEL ISSUE, unnecessary transforms present
-    private venus!: IGameObject //==> OK, but unnecessary transforms present
-    private mars!: IGameObject //==> OK, but unnecessary transforms present (it`s off center for whatever reason)
-    private saturn!: IGameObject //==> OK, but the model is huge
+    private earth!: IGameObject //==> DONE
+    private uranus!: IGameObject //==> DONE
+    private jupiter!: IGameObject //==> DONE
+    private mercury!: IGameObject //==> DONE
+    private venus!: IGameObject //==> DONE
+    private mars!: IGameObject //==> DONE
+    private saturn!: IGameObject //==> DONE
     private neptune!: IGameObject //==> OK, needs optimization
 
     constructor() {
         super()
+        this.camera = new ArcBallCamera({
+            zFar: 1000,
+            zNear: 0.1,
+            canvasWidth: this._engine!.context.canvas.width,
+            canvasHeight: this._engine!.context.canvas.height,
+            position: vec3.create(0, 0, 5),
+        })
         this._shaderModule = this.engine.device.createShaderModule({
             code: shaderCode,
         })
     }
 
-    public async init() {
-        // await Promise.all(
-        //     stuffNames.map((name) => {
-        //         WheezyGLBLoader.loadFromUrl(`static/models/planets/${name}.glb`)
-        //             .then((modelData) => {
-        //                 this.uploadModel({
-        //                     modelData,
-        //                     shaderModule: this._shaderModule,
-        //                 }).then((gameObject) => {
-        //                     ;(this as any)[name] = gameObject
-        //                 })
-        //             })
-        //             .catch(console.error)
-        //     })
-        // )
+    private async _setupSun() {
         const planetMD = await WheezyGLBLoader.loadFromUrl(
-            'static/models/planets/neptune.glb'
+            'static/models/planets/sun.glb'
         )
         this.sun = await this.uploadModel({
             modelData: planetMD,
             shaderModule: this._shaderModule,
         })
-        this.sun.transform.rotateDegreesEuler({ x: 90 })
+
+        console.log(this.objectManager)
+
+        this.sun.transform.scale(vec3.create(0.1, 0.1, 0.1))
     }
 
-    protected onRender(deltaTime: number): void {
-        this.sun.transform.rotateDegreesEuler({ z: 3 * deltaTime })
+    private async _setupMercury() {
+        const planetMD = await WheezyGLBLoader.loadFromUrl(
+            'static/models/planets/mercury.glb'
+        )
+        this.mercury = await this.uploadModel({
+            modelData: planetMD,
+            shaderModule: this._shaderModule,
+        })
+
+        console.log(`sun ${this.sun.id}`)
+        console.log(`mercury ${this.mercury.id}`)
+
+        this.objectManager.reparentObject(this.mercury, this.sun)
+
+        console.log(this.objectManager)
     }
+
+    // private async _setupVenus() {
+    //     const planetMD = await WheezyGLBLoader.loadFromUrl(
+    //         'static/models/planets/venus.glb'
+    //     )
+    //     this.venus = await this.uploadModel({
+    //         modelData: planetMD,
+    //         shaderModule: this._shaderModule,
+    //     })
+    // }
+
+    public async init() {
+        await this._setupSun()
+        await this._setupMercury()
+        // await this._setupVenus()
+    }
+
+    protected onRender(deltaTime: number): void {}
 }
