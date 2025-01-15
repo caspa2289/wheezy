dc9fa644b68a38707c382ddc7f13474e82ed7f76
caspa <example@example.com>
2025-01-15 15:43:54 +0300

    fixed incorrect normal texture initialization

--- packages/demos/index.ts
+++ packages/demos/index.ts
@@ -5,6 +5,9 @@ import { Demo1 } from './src/Demo1'
 const canvas = document.getElementById('webgpu-canvas') as HTMLCanvasElement
 
 const initDebugControls = (engine: IEngine) => {
+    const defaultOutputSource = 0
+    const defaultRenderingMode = 1
+
     const debugContainer = document.getElementById(
         'debug-container'
     ) as HTMLDivElement
@@ -35,6 +38,9 @@ const initDebugControls = (engine: IEngine) => {
     ]
     outputOptions.forEach((item) => {
         const option = document.createElement('option')
+        if (item.value === defaultOutputSource) {
+            option.selected = true
+        }
         option.textContent = item.label
         option.value = String(item.value)
         outputSourceDropDown.appendChild(option)
@@ -47,13 +53,16 @@ const initDebugControls = (engine: IEngine) => {
 
     renderModes.forEach((item) => {
         const option = document.createElement('option')
+        if (item.value === defaultRenderingMode) {
+            option.selected = true
+        }
         option.textContent = item.label
         option.value = String(item.value)
         renderingModeDropDown.appendChild(option)
     })
 
-    engine.renderer.outputSource = 0
-    engine.renderer.renderingMode = 0
+    engine.renderer.outputSource = defaultOutputSource
+    engine.renderer.renderingMode = defaultRenderingMode
 
     debugContainer.appendChild(outputSourceDropDown)
     debugContainer.appendChild(renderingModeDropDown)
